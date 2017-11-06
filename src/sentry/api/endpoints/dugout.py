6b84389e5e69f0da862d3cb19650ab89457d5506
Eric Feng <erichfeng@gmail.com>
2017-11-06 11:52:03 -0800

    mock guide

--- src/sentry/api/endpoints/dugout.py
+++ src/sentry/api/endpoints/dugout.py
@@ -10,4 +10,28 @@ class DugoutEndpoint(Endpoint):
     permission_classes = (IsAuthenticated, )
 
     def get(self, request):
-        return Response({})
+        request.GET.get('url')
+        platform = 'python'
+        project_name = 'Sentry Backend'
+        org_slug = 'sentry'
+        project_slug = 'sentry'
+
+        result = {
+            'id': 'setup-release-tracking',
+            'starting_url': 'https://sentry.io/sentry/sentry/',
+            'steps': [
+                {
+                    'title': 'You sent your %s first event!' % platform,
+                    'description': 'Silence alerts for issues you\'ve fixed. Set up release tracking to mark issues as "resolved in next release."',
+                    'target': '.btn.project-settings',
+                    'event': 'click',
+                },
+                {
+                    'title': 'Click on Release Tracking',
+                    'description': 'Project Settings is where you configure your %s project.' % project_name,
+                    'target': '[href=%s/%s/settings/release-tracking/]' % (org_slug, project_slug),
+                    'event': 'click',
+                }
+            ]
+        }
+        return Response(result)
