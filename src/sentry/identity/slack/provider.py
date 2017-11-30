eac64c11c461eed55706cfea0af6d08f88e7ef10
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-11-30 11:29:46 -0800

    feat: Implement generic identity association pipeline

---
+++ src/sentry/identity/slack/provider.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+
+from sentry import options
+from sentry.identity.oauth2 import OAuth2Provider
+
+options.register('slack.client-id')
+options.register('slack.client-secret')
+options.register('slack.verification-token')
+
+
+class SlackIdentityProvider(OAuth2Provider):
+    key = 'slack'
+    name = 'Slack'
+
+    oauth_access_token_url = 'https://slack.com/api/oauth.access'
+    oauth_authorize_url = 'https://slack.com/oauth/authorize'
+
+    oauth_scopes = (
+        'identity.basic',
+        'identity.email',
+    )
+
+    def get_oauth_client_id(self):
+        return options.get('slack.client-id')
+
+    def get_oauth_client_secret(self):
+        return options.get('slack.client-secret')
+
+    def build_identity(self, data):
+        data = data['data']
+
+        return {
+            'type': 'slack',
+            'id': data['user']['id'],
+            'email': data['user']['email'],
+            'scopes': sorted(data['scope'].split(',')),
+            'data': self.get_oauth_data(data),
+        }
